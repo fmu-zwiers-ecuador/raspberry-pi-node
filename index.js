c8d3e5394a334e67c17eb63b6522f91339d12920
AndersonL <lorealanderson@me.com>
2018-10-15 14:02:50 -0400

    Added downloads

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require('express')
 var app = express()
 
+
 console.log('Initializing Raspberry Pi Node');
 
 // Define Node Endpoints
@@ -24,20 +25,19 @@ app.get('/api', function (req, res) {
 
 app.get('/api/video', function (req, res) {
 
-    var videos = [ "video1.mp4", "video2.mp4", "video3.mp4" ];
+   // var videos = [ "video1.mp4", "video2.mp4", "video3.mp4" ];
+   const fs = require("fs");
 
-    res.send(videos);
-
-})
-
-app.get('/api/video/:filename', function (req, res) {
-
-    var fileName = req.params.filename;
-
-    res.send("Downloading video content for file: " + fileName);
+   let directory = "/Users/lorealanderson/Desktop";
+   let dirBuf = Buffer.from(directory);
+   let files = fs.readdirSync(directory, '');
+   console.log(files);
+    //array of files on the pc
+    res.send(files);
 
 })
 
+//fs sys module
 app.delete('/api/video/:filename', function (req, res) {
 
     var fileName = req.params.filename;
@@ -55,6 +55,10 @@ app.get('/api/sensors', function (req, res) {
     res.send(sensors);
     
 })
+
+// ExpressJS serve static files
+app.use('/api/download', express.static('/Users/lorealanderson/Desktop'));
+//app.use('/api/download', express.static('/home/pi/Desktop/videos'));
 
 // Start Express Web Server
 app.listen(3000)
